4a1d63eba02c7ca91f3cf37cb5c545225ddf6726
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-10-02 14:11:42 +0530

    creating getter and setter methods

--- src/Main/Question11.java
+++ src/Main/Question11.java
@@ -5,4 +5,17 @@ public class Question11 {
 class Ques11{
 private String str;
 private char chr;
+    public String getStr(){
+    return str;
+}
+    public char getChr(){
+    return chr;
+    }
+    public void setStr(String str){
+    this.str=str;
+    }
+    public void setChr(){
+    this.chr=chr;
+    }
+
 }
