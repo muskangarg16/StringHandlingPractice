44d27c348eb4fb1a57753419d3da7cab886c0ae4
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-10-05 21:56:51 +0530

    defining definition  class method occur

--- src/Main/Question4.java
+++ src/Main/Question4.java
@@ -1,7 +1,20 @@
 package Main;
 
 public class Question4 {
+
 }
 class Ques4{
 private String s;
+public Ques4(String s) {
+    this.s = s;
+}
+    public String getStr() {
+        return s;
+    }
+    public void setStr(String s) {
+        this.s = s;
+    }
+    public String occur(){
+
+    }
 }
