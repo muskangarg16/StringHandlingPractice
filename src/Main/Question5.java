11d2b5f3299fb6b0ac6b23c38501358a00f515ae
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-10-05 22:13:22 +0530

    defining definition method getter and setter method

--- src/Main/Question5.java
+++ src/Main/Question5.java
@@ -2,10 +2,18 @@ package Main;
 
 public class Question5 {
 }
-class Ques5{
+class Ques5 {
     private String s;
     public Ques5(String s){
         this.s=s;
     }
-
+    public String getStr() {
+        return s;
+    }
+    public void setStr(String s) {
+        this.s = s;
+    }
+    public String replaceSubstring{
+        
+    }
 }
