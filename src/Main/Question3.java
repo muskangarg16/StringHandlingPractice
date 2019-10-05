2e8557a97bfa808b9278328e3c154e0f79bfb569
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-10-05 20:43:54 +0530

    init commit

--- src/Main/Question3.java
+++ src/Main/Question3.java
@@ -1,21 +1,38 @@
 package Main;
+import org.w3c.dom.ls.LSOutput;
 
+import java.util.*;
 public class Question3 {
-}
-class Ques3{
-    private String str;
-    public Ques3(String str){
-        this.str=str;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        Ques3 obj = new Ques3(str);
+        System.out.println(obj.replaceString());
     }
+}
+class Ques3 {
+    private String str;
+
+    public Ques3(String str) {
+        this.str = str;
+    }
+
     public String getStr() {
         return str;
     }
+
     public void setStr(String str) {
         this.str = str;
     }
-    public String replaceString(){
-        String t=getStr();
-        t=getStr().replace("aeiouAeiou","*");
-        return t;
+
+    public String replaceString() {
+        String t = getStr();
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U') {
+                t=t.replace(c, '*');
+            }
+        }
+            return t;
     }
 }
