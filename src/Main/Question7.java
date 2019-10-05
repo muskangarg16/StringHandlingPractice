10f008d37108e45a2e843b02ae93c59ea9b11737
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-10-05 21:37:40 +0530

    defining definition class totweight() method

--- src/Main/Question7.java
+++ src/Main/Question7.java
@@ -6,6 +6,18 @@ class Ques7{
     private String str;
     Ques7(String str){
     this.str=str;
-}
-
+    }
+    public String getStr() {
+        return str;
+    }
+    public void setStr(String str) {
+        this.str = str;
+    }
+    public int totweight(){
+        int a=0;
+        for(int i=0;i<getStr().length();i++){
+            a=a+(int)s.charAt(i);
+        }
+        return a;
+    }
 }
