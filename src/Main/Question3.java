0414965d390fbf5dec177ffeceec4b9d46b350fd
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-10-05 19:44:36 +0530

    defining definition class getter and setter method

--- src/Main/Question3.java
+++ src/Main/Question3.java
@@ -7,5 +7,10 @@ class Ques3{
     public Ques3(String str){
         this.str=str;
     }
-
+    public String getStr() {
+        return str;
+    }
+    public void setStr(String str) {
+        this.str = str;
+    }
 }
