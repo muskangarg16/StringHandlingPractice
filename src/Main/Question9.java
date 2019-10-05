ae3f79e52ce81d2fa78b79c99b7f8edc8e8c54f5
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-10-05 18:43:00 +0530

    init commit

--- src/Main/Question9.java
+++ src/Main/Question9.java
@@ -1,7 +1,12 @@
 package Main;
-
+import java.util.Scanner;
 public class Question9 {
-
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        Ques9 obj = new Ques9(str);
+        System.out.println(obj.AbsWeight());
+    }
 }
 class Ques9{
     String str;
@@ -15,6 +20,17 @@ class Ques9{
         this.str = str;
     }
     public int AbsWeight(){
-
+        int s = 0;
+        int d=0;
+        for (int i = 0; i < getStr().length(); i++) {
+            if (getStr().charAt(i) >= 'A' && getStr().charAt(i) <= 'Z') {
+                s = s + (int) getStr().charAt(i);
+            }
+            else{
+                d=d+(int) getStr().charAt(i);;
+            }
+        }
+        int totalweight=Math.abs(s-d);
+        return totalweight;
     }
 }
