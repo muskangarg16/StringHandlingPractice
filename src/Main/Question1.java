c371ce76a725c79b35f8b0d2638f9a285efe987b
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-10-05 19:23:44 +0530

    creating stringFormat method

--- src/Main/Question1.java
+++ src/Main/Question1.java
@@ -7,19 +7,31 @@ public class Question1 {
     Scanner in=new Scanner(System.in);
     String s=in.nextLine();
     Ques1 obj=new Ques1(s);
-    System.out.println(obj.stringFormat());
+    obj.stringFormat(s);
     }
 
 }
 class Ques1{
     private String s;
-    public Ques8(String s) {
+    public Ques1(String s) {
         this.s = s;
     }
     public String getStr() {
         return s;
     }
-    public void setStr(String str) {
-        this.s = str;
+    public void setStr(String s) {
+        this.s = s;
+    }
+    public String stringFormat(String s){
+        System.out.print("index:"+"\t");
+        for(int i=0;i<s.length();i++) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
+        System.out.print("chars:"+"\t");
+        for(int i=0;i<s.length();i++){
+            System.out.print(s.charAt(i)+"\t");
+        }
+        return "";
     }
 }
