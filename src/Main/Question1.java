527be9e481895c832e07c526677a7fe0f8194f03
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-10-05 18:47:38 +0530

    creating constructor of definition class

--- src/Main/Question1.java
+++ src/Main/Question1.java
@@ -1,11 +1,19 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Question1 {
     public static void main(String[] args) {
-
+    Scanner in=new Scanner(System.in);
+    String s=in.nextLine();
+    Ques1 obj=new Ques1(s);
+    System.out.println(obj.stringFormat());
     }
 
 }
 class Ques1{
-
+    private String str;
+    public Ques8(String str) {
+        this.str = str;
+    }
 }
