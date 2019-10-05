6ec1c518c7c76819557fa179c66c23bde5d14919
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-10-05 21:44:39 +0530

    defining main method

--- src/Main/Question6.java
+++ src/Main/Question6.java
@@ -1,7 +1,24 @@
 package Main;
 
 public class Question6 {
+    public static void main(String[] args) {
+
+    }
 }
 class Ques6{
 private String str;
+public Ques6(String str){
+    this.str=str;
+}
+    public String getStr() {
+        return str;
+    }
+    public void setStr(String s) {
+        this.str = str;
+    }
+    public String upper(){
+    String q=getStr();
+    q=q.toUpperCase();
+    return q;
+    }
 }
