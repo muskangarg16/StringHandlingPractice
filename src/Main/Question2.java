7bad842f8b8ef456457f8ef8f939ee6ed0b8d3ac
muskangarg16 <muskan.garg_cs18@gla.ac.in>
2019-10-05 19:32:40 +0530

    creating definition class stringSubstring method

--- src/Main/Question2.java
+++ src/Main/Question2.java
@@ -1,16 +1,31 @@
 package Main;
 
 public class Question2 {
+
 }
 class Ques2{
 private String str;
+private int index;
     public Ques2(String str) {
         this.str = str;
     }
     public String getStr() {
         return str;
     }
+    public int getIndex() {
+        return index;
+    }
+    public void setIndex(int index) {
+        this.index = index;
+    }
     public void setStr(String str) {
         this.str = str;
+    }
+    public String stringSubstring(){
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<getStr().length();i++){
+            sb.append(getStr().substring(getIndex()));
+        }
+        return sb.toString();
     }
 }
